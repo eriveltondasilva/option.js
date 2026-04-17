04cb823175be94fa8d2223fa353fe7048faf8544
Erivelton Silva <eriveltondasilva13@gmail.com>
2026-04-17 09:45:55 -0300

    organiza a estrutura da interface OptionMethods

--- src/types.ts
+++ src/types.ts
@@ -1,40 +1,50 @@
 export interface OptionMethods<T> {
-  // Verificação
+  // Type Guards
   isSome(): this is Some<T>
   isNone(): this is None
   isSomeAnd(predicate: (value: T) => boolean): boolean
   isNoneOr(predicate: (value: T) => boolean): boolean
 
-  // Extração
+  // Extraction
   unwrap(): T
   expect(message: string): T
   unwrapOr<U>(defaultValue: U): T | U
   unwrapOrElse<U>(fn: () => U): T | U
 
-  // Transformação
+  // Transformation
   map<U>(fn: (value: T) => U): Option<U>
   mapOr<U>(fn: (value: T) => U, defaultValue: U): U
   mapOrElse<U>(fn: (value: T) => U, defaultFn: () => U): U
   filter(predicate: (value: T) => boolean): Option<T>
   flatten<U>(this: Option<Option<U>>): Option<U>
 
-  // Alternativas
+  // Alternation
   and<U>(other: Option<U>): Option<U>
   andThen<U>(fn: (value: T) => Option<U>): Option<U>
   or<U>(other: Option<U>): Option<T | U>
   orElse<U>(fn: () => Option<U>): Option<T | U>
 
-  // Inspeção
-  match<U>(patterns: { some: (value: T) => U; none: () => U }): U
-  inspect(fn: (value: T) => void): Option<T>
+  // Combination
+  zip<U>(other: Option<U>): Option<[T, U]>
+  zipWith<U, R>(other: Option<U>, fn: (a: T, b: U) => R): Option<R>
 
-  // Conversão
+  // Inspection
+  match<U>(handlers: { some: (value: T) => U; none: () => U }): U
+  inspect(fn: (value: T) => void): Option<T>
+  tap(fn: (value: T) => void): Option<T>
+
+  // Conversion
   toNullable(): T | null
+  toUndefined(): T | undefined
+
+  // TODO: implement these methods
+  // toString(): string
+  // toValue(): T
+  // toJSON(): T
 }
 
 export interface Some<T> extends OptionMethods<T> {
   readonly _tag: 'Some'
-  readonly value: T
 }
 
 export interface None extends OptionMethods<never> {
@@ -42,3 +52,5 @@ export interface None extends OptionMethods<never> {
 }
 
 export type Option<T> = Some<T> | None
+
+export type AsyncOption<T> = Promise<Option<T>>
