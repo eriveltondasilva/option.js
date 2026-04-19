245b4ccfce4aed0836d4f8a7c3ee010e19a8c96d
Erivelton Silva <eriveltondasilva13@gmail.com>
2026-04-19 17:31:01 -0300

    refatora: ajusta importações e melhora a imutabilidade do objeto Option

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { option } from './option.ts'
-import type { IOption } from './types.ts'
+import * as option from './option.ts'
+import type { IAsyncOption, IOption } from './types.ts'
 
 export { NoneClass as None } from './none.ts'
 export { Some } from './some.ts'
 
 export type Option<T> = IOption<T>
-export const Option = Object.freeze(option)
+export type AsyncOption<T> = IAsyncOption<T>
+
+export const Option = Object.freeze({ ...option })
 export default Option
