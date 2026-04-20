eecc506d1ec64e01a576e2504db9936dc69dfa28
Erivelton Silva <eriveltondasilva13@gmail.com>
2026-04-20 01:43:58 -0300

    refatora: remove captura de stack trace da classe NoneUnwrapError

--- src/errors.ts
+++ src/errors.ts
@@ -2,9 +2,5 @@ export class NoneUnwrapError extends Error {
   constructor(message = 'Called unwrap() on a None value') {
     super(message)
     this.name = 'NoneUnwrapError'
-
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, NoneUnwrapError)
-    }
   }
 }
